b17ffa2962ff90267a101facd66eb233f2a2110d
Jakub <kuba.reszel@gmail.com>
2024-08-05 21:33:03 +0200

    Add Validation to the Movie Class

--- src/main/java/com/gitDemo/movieLibraryDemo/Movie.java
+++ src/main/java/com/gitDemo/movieLibraryDemo/Movie.java
@@ -1,14 +1,23 @@
 package com.gitDemo.movieLibraryDemo;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
     private int id;
+
+    @NotNull(message = "Title cannot be null")
+    @Size(min = 1, max = 100, message = "Title must be between 1 and 100 characters")
     private String title;
+
+    @Min(value = 0, message = "Rating must be at least 0")
     private int rating;
 }
