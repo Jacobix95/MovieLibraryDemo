dd759fb9635c7c36ce032e85a17813b6919393a8
Jakub <kuba.reszel@gmail.com>
2024-12-19 18:32:15 +0100

    Minor changes

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ It is a CRUD application that demonstrates skills related to database management
 Java 21
 # Technologies used
 - MySQL
-- Docker
+- Hibernate
+- Docker (IN PROGRESS)
 - Flyway
 - Spring Boot
 - JDBC API
--- src/main/resources/db/migration/mysql/V1_0__schema.sql
+++ src/main/resources/db/migration/mysql/V1_0__schema.sql
@@ -1,9 +1,10 @@
-CREATE TABLE movie (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       title VARCHAR(200) NOT NULL,
-                       director VARCHAR(200),
-                       release_year INT,
-                       genre VARCHAR(200),
-                       rating INT,
-                       version INT
+CREATE TABLE movie
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title        VARCHAR(200) NOT NULL,
+    director     VARCHAR(200),
+    release_year INT,
+    genre        VARCHAR(200),
+    rating       INT,
+    version      INT
 );
